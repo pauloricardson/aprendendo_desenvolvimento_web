2054c8808d0b0c6ef28f59f1b24fe3d728b1a3ec
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-03 10:23:35 -0300

    Diferença entre LET, VAR e CONST em Javascript - Curso de Javascript Moderno - Aula 03

---
+++ aprendendo_javascript/cfb-cursos/aula003/script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+/*
+function teste(){
+    if(true){
+        let nome="bruno";
+        console.log("Dentro do IF do teste: " + nome);
+    }
+    console.log("Dentro de teste: " + nome);
+}
+
+teste();
+
+console.log("fora de teste: " + nome);
+*/
+
+let nome = "Paulo";
+nome = 10;
+
+const curso = "Javascript";
+
+console.log(curso);
+
+curso = "Paulo"; // não vai alterar, vai dar erro
