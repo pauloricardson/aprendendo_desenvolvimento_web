e01ef2c33d3357e7c683c85ed0d784ee8ef1a90b
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-04 10:06:05 -0300

    BR
    Aprendendo sobre operador ternário - Curso de Javascript Moderno - Aula 10

---
+++ aprendendo_javascript/cfb-cursos/aula010/script.js
@@ -0,0 +1,23 @@
+// operador ternário
+
+let num1 = 10;
+let num2 = 20;
+
+// let res = num % 2;
+
+// if (res == 0){
+//     console.log("PAR");
+// }else{
+//     console.log("IMPAR");
+// }
+
+res = (!(num1 % 2) ? "PAR" : "IMPAR");
+
+console.log(res);
+
+res = num1 > num2 ? "SIM" : "NÃO";
+
+console.log(res);
+
+
+// teste lógico ? se verdadeiro : se falso
