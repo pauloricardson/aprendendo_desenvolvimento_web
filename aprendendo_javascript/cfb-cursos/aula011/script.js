48ef419c096b9fcc151adb0f977bc9527e7914eb
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-04 10:18:20 -0300

    BR
    Operador Typeof, retornando o tipo da variável - Curso de Javascript Moderno - Aula 11

---
+++ aprendendo_javascript/cfb-cursos/aula011/script.js
@@ -0,0 +1,9 @@
+let v1 = 1;
+let v2 = "10";
+let v3 = v1 === v2;
+let v4 = {nome: "Paulo"};
+
+console.log(typeof(v1)); // number
+console.log(typeof(v2)); // String
+console.log(typeof(v3)); // Boolean
+console.log(typeof(v4)); // Object
