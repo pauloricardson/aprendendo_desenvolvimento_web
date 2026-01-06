e10dda37395ff93a239c93c2f33d1ccb8d66058b
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-06 16:13:20 -0300

    for in e for of

---
+++ aprendendo_javascript/aula016/script.js
@@ -0,0 +1,13 @@
+let num = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < num.length; i++){
+//     console.log(num[i]);
+// }
+
+for (n of num) {
+    console.log(n);
+}
+
+for (n in num) {
+    console.log(num[n]);
+}
