a8b7dab131de954fda5207f44d2e4ee027ec978c
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-07 10:54:38 -0300

    Aprendendo sobre FUNÇÕES em Javascript

---
+++ aprendendo_javascript/aula020/script.js
@@ -0,0 +1,17 @@
+// function soma(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(soma(5, 5));
+// console.log(soma(1, 5));
+// console.log(soma(2, 5));
+
+function mudarTexto() {
+    let d1 = document.getElementById("d1");
+    let d2 = document.getElementById("d2");
+    let d3 = document.getElementById("d3");
+
+    d1.innerHTML = "Texto trocado";
+    d2.innerHTML = "Texto trocado";
+    d3.innerHTML = "Texto trocado";
+}
