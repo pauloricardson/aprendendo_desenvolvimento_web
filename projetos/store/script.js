108d358af59ae3dd1c1aee8c2be6f9e965dd4780
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-06 23:06:39 -0300

    atualização

--- projetos/store/script.js
+++ projetos/store/script.js
@@ -2,4 +2,11 @@ const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const items = document.querySelectorAll('.item');
 const dots = document.querySelectorAll('dot');
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list');
+
+let active = 0;
+const total = items.length;
+
+let timer;
 
