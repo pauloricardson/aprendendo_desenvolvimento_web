a08ed79dfac1523344daecf5f57b09fa7aa8d4e8
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-06 23:01:48 -0300

    contantes

---
+++ projetos/store/script.js
@@ -0,0 +1,5 @@
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const items = document.querySelectorAll('.item');
+const dots = document.querySelectorAll('dot');
+
