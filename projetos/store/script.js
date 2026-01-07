8fc348b1e024b81942259ee1e9f2f0165235a74c
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-06 23:33:55 -0300

    fazendo o carrossel

--- projetos/store/script.js
+++ projetos/store/script.js
@@ -1,7 +1,7 @@
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const items = document.querySelectorAll('.item');
-const dots = document.querySelectorAll('dot');
+const dots = document.querySelectorAll('.dot');
 const numberIndicator = document.querySelector('.numbers')
 const list = document.querySelector('.list');
 
@@ -12,7 +12,20 @@ let timer;
 
 
 function update(direction) {
-    
+    document.querySelector('.item.active').classList.remove('active');
+    document.querySelector('.dot.active').classList.remove('active');
+
+    if (direction > 0) {
+        active = active + 1;
+        if (active === total) {
+            active =0;
+        }
+    } else if (direction < 0) {
+
+    }
+
+    items[active].classList.add('active');
+    dots[active].classList.add('active');
 }
 
 
