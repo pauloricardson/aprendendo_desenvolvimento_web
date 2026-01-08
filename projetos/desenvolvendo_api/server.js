6660a6862f995318394c8981126903dbad1bd4da
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-07 21:22:40 -0300

    atualizacao

---
+++ projetos/desenvolvendo_api/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+    res.send('Ok post')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.json(users)
+});
+
+app.listen(3000)
