1c68295614e07752f7de71efde76e63c3b287545
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-08 11:30:35 -0300

    arrow function

--- aprendendo_javascript/aula025/script.js
+++ aprendendo_javascript/aula025/script.js
@@ -1,2 +1,12 @@
 // Funções Lambidas - Arrow Funcition
 
+const soma = (v1, v2) => { return v1 + v2 }
+
+const nome = n => {return n}
+
+const add = n => n + 10;
+
+
+console.log(soma(2, 5));
+
+console.log(nome("bruno"));
