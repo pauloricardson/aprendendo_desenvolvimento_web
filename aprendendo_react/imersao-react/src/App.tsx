66bd36c42c9933d79ec12047619657460c4cd6f6
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-09 12:16:09 -0300

    estilzando o header

---
+++ aprendendo_react/imersao-react/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css"
+
+function App() {
+  return <div className="minha-div">Odair</div>;
+}
+
+export default App;
---
+++ aprendendo_react/imersao-react/src/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <img className="logo" src="./Logo.png" alt="" />
+            <p className="btn-premium">Premium</p>
+        </div>
+    );
+}
+
+export default Header;
