e7fe2af51ef0c401e81af4272f6a97b06a64e5b6
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-10 20:21:31 -0300

    Aprendendo sobre o método MAP em Javascript

--- aprendendo_javascript/aula028/script.js
+++ aprendendo_javascript/aula028/script.js
@@ -1,7 +1,31 @@
-const cursos = ["HTML", "CSS", "JAVASCRIPT"];
+// const el = document.getElementsByTagName("div");
 
-let c = cursos.map((el, i) => {
-    return "<div>" + el + "<div/>";
-})
+const converterInt = (e) => e * 2;
 
-console.log(c);
+let num = ["1", "2", "3"].map(converterInt);
+console.log(num);
+
+// const val = Array.prototype.map.call(el, ({innerHTML}) => innerHTML)
+// console.log(val);
+
+
+// let el = document.getElementsByTagName("div");
+
+// el = [...el];
+// console.log(el);
+
+// el.map((e, i) => {
+//     e.innerHTML = "CFB";
+//     console.log(e);
+// })
+
+
+
+
+// const cursos = ["HTML", "CSS", "JAVASCRIPT"];
+
+// let c = cursos.map((el, i) => {
+//     return el;
+// })
+
+// console.log(c);
