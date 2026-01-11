052d265e8338a18fce99139b7bf460e1b9d57d2f
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-10 23:41:27 -0300

    outra maneira de fazer

--- aprendendo_javascript/exercicios/ex005.js
+++ aprendendo_javascript/exercicios/ex005.js
@@ -1,16 +1,20 @@
-function checkSpeed(speed) {
-    if (speed <= 65) {
-        console.log(speed);
-    } else if (speed >= 66 && speed <= 70) {
-        console.log("Points: 1");
-    } else if (speed > 70 && speed <= 75) {
-        console.log("Points: 2");
-    } else if (speed > 75 && speed <= 80) {
-        console.log("Points: 3");
-    } else if (speed > 80 && speed <= 85) {
-        console.log("Points: 3");
-    }
-}
+// function checkSpeed(speed) {
+//     if (speed <= 65) {
+//         console.log(speed);
+//     } else if (speed >= 66 && speed <= 70) {
+//         console.log("Points: 1");
+//     } else if (speed > 70 && speed <= 75) {
+//         console.log("Points: 2");
+//     } else if (speed > 75 && speed <= 80) {
+//         console.log("Points: 3");
+//     } else if (speed > 80 && speed <= 85) {
+//         console.log("Points: 4");
+//     } else if (speed > 85 && speed <= 90){
+//         console.log("Points: 5");
+//     } else {
+//         console.log("Licence suspended");
+//     }
+// }
 
-let result = checkSpeed(65);
-console.log(result);
+// checkSpeed(100);
+
