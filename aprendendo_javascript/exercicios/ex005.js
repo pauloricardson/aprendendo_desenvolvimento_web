afc42e9a837aaa4a527473ae858d560f9b372724
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-10 23:39:28 -0300

    atualização

--- aprendendo_javascript/exercicios/ex005.js
+++ aprendendo_javascript/exercicios/ex005.js
@@ -2,8 +2,14 @@ function checkSpeed(speed) {
     if (speed <= 65) {
         console.log(speed);
     } else if (speed >= 66 && speed <= 70) {
-        console.log("Points: ", 1);
-    } else if (speed > 70 && speed <=)
+        console.log("Points: 1");
+    } else if (speed > 70 && speed <= 75) {
+        console.log("Points: 2");
+    } else if (speed > 75 && speed <= 80) {
+        console.log("Points: 3");
+    } else if (speed > 80 && speed <= 85) {
+        console.log("Points: 3");
+    }
 }
 
 let result = checkSpeed(65);
