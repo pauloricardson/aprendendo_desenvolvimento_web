810e2b4db2cd3f84085b4a158af8e39be8017747
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-11 09:13:38 -0300

    Aprendendo o Método getElementById

--- aprendendo_javascript/aulas/aula030/script.js
+++ aprendendo_javascript/aulas/aula030/script.js
@@ -1,7 +1,17 @@
 const c1 = document.getElementById("c1");
-const c2 = docment.ge
-console.log(c1);
-console.log(c1.id);
-console.log(c1.innerHTML);
+const c2 = document.getElementById("c2");
+const c3 = document.getElementById("c3");
+const c4 = document.getElementById("c4");
+const c5 = document.getElementById("c5");
+const c6 = document.getElementById("c6");
 
-c1.innerHTML = "alterado";
+const arrayElementos = [c1, c2, c3, c4, c5, c6];
+
+// for (d of arrayElementos)
+//     d.innerHTML = "curso"
+
+arrayElementos.map((el) => {
+    el.innerHTML = "outro conteudo";
+})
+
+// console.log(arrayElementos);
