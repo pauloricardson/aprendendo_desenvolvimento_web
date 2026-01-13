7d545fbef075a23beb3705ff5799684d3323d522
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-13 19:27:03 -0300

    ATUAZLIAÇÃO DE DOC

--- aprendendo_javascript/revisao/revendo-funcoes/getElementById.js
+++ aprendendo_javascript/revisao/revendo-funcoes/getElementById.js
@@ -1,5 +1,12 @@
 const dc1 = document.getElementById("c1");
+const dc2 = document.getElementById("c2");
+const dc3 = document.getElementById("c3");
+const dc4 = document.getElementById("c4");
+const dc5 = document.getElementById("c5");
+const dc6 = document.getElementById("c6");
 
-console.log(dc1);
-console.log(dc1.id);
-console.log(dc1.innerHTML);
+const elementosDiv = [dc1, dc2, dc3, dc4, dc5, dc6];
+
+elementosDiv.map((e) => {
+    e.innerHTML = 'alterando elementos';
+});
