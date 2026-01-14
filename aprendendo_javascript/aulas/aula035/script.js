a205485582026a4e614ebe3d39f27ddb906f0a6c
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2026-01-14 17:21:35 -0300

    atualização da atividade

--- aprendendo_javascript/aulas/aula035/script.js
+++ aprendendo_javascript/aulas/aula035/script.js
@@ -1,11 +1,16 @@
 const caixa1 = document.querySelector("#caixa1");
 const caixa2 = document.querySelector("#caixa2");
-const btn = document.querySelector("#btn-copiar");
+const btn = document.querySelector("#btn-transferir");
 const todosCursos = [...document.querySelectorAll(".curso")];
 
 todosCursos.map((el) => {
     el.addEventListener("click", (evt) => {
         const curso = evt.target;
+
+        if (caixa2.contains(curso)) {
+            caixa1.appendChild(curso);
+        }
+
         curso.classList.toggle("seletor");
     })
 })
