d34655cfc364358b58643ce462dd492ed95b696d
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2026-01-14 13:12:10 -0300

    atualização

---
+++ aprendendo_javascript/revisao/revendo-funcoes/15-addEventListenner.js
@@ -0,0 +1,8 @@
+const todosCursos = [...document.querySelectorAll(".curso")];
+
+todosCursos.map((el) => {
+    el.addEventListener('click', (evt) => {
+        const selec = evt.target;
+        selec.classList.add('destaque');
+    })
+})
