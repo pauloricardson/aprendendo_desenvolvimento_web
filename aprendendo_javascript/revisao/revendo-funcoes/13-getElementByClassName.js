07746ed78934db22b453a9b33da845312c93ce89
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-13 22:55:45 -0300

    atualizando

--- aprendendo_javascript/revisao/revendo-funcoes/13-getElementByClassName.js
+++ aprendendo_javascript/revisao/revendo-funcoes/13-getElementByClassName.js
@@ -3,9 +3,7 @@ const cursosTodos = [...document.getElementsByClassName('curso')];
 const cursoC1 = [...document.getElementsByClassName('C1')];
 const cursoC2 = [...document.getElementsByClassName('C2')];
 
-console.log(cursoC1)
-console.log(cursoC2)
 
-cursoC2.map((el) => {
-    el.classList.add('destaque');
-})
+const primeiroCurso = document.getElementsByClassName('curso')[6];
+
+console.log(primeiroCurso)
