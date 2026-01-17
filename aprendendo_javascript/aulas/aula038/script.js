ed37411744b67a5789bf74f52574984288f9def0
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-17 17:35:34 -0300

    atualizacao

--- aprendendo_javascript/aulas/aula038/script.js
+++ aprendendo_javascript/aulas/aula038/script.js
@@ -1,2 +1,6 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
+
+
+console.log(btn_c[0].getRootNode())
+console.log(btn_c[0].ownerDocument)
