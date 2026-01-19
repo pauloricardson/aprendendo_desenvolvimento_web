bfb4ff3c548c0a54837bdc79d12f48ebdee1996d
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 18:55:45 -0300

    atualização

--- aprendendo_javascript/aulas/aula040/script.js
+++ aprendendo_javascript/aulas/aula040/script.js
@@ -2,5 +2,8 @@ const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 
 const novoElemento = document.createElement("div")
+novoElemento.setAttribute("id", "c7")
+novoElemento.setAttribute("class", "curso c1")
+novoElemento.innerHTML = "ReactNative"
 
 caixa1.appendChild(novoElemento)
