18bc9606dcecfc5624118290b5bc928f89bd74fb
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 19:05:26 -0300

    adicionando elementos

--- aprendendo_javascript/aulas/aula040/script.js
+++ aprendendo_javascript/aulas/aula040/script.js
@@ -1,9 +1,18 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
+const cursos = ['teste' ,'HTML', 'CSS', 'Javascrip', 'PHP', 'React', 'MySQL', 'ReactNative']
 
-const novoElemento = document.createElement("div")
-novoElemento.setAttribute("id", "c7")
-novoElemento.setAttribute("class", "curso c1")
-novoElemento.innerHTML = "ReactNative"
+// const novoElemento = document.createElement("div")
+// novoElemento.setAttribute("id", "c7")
+// novoElemento.setAttribute("class", "curso c1")
+// novoElemento.innerHTML = "ReactNative"
 
-caixa1.appendChild(novoElemento)
+// caixa1.appendChild(novoElemento)
+
+cursos.map((el, i) => {
+    const novoElemento = document.createElement("div");
+    novoElemento.setAttribute("id", "c" + (i + 1));
+    novoElemento.setAttribute("class", "curso c1")
+    novoElemento.innerHTML = (el)
+    caixa1.appendChild(novoElemento)
+})
