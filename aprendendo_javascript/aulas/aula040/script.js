c8493f0b00f4e337bde3ed6170b2ac291f64baa3
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 19:10:56 -0300

    Criando novos elementos e adicionando no DOM em Javascript

--- aprendendo_javascript/aulas/aula040/script.js
+++ aprendendo_javascript/aulas/aula040/script.js
@@ -2,12 +2,7 @@ const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 const cursos = ['teste' ,'HTML', 'CSS', 'Javascrip', 'PHP', 'React', 'MySQL', 'ReactNative']
 
-// const novoElemento = document.createElement("div")
-// novoElemento.setAttribute("id", "c7")
-// novoElemento.setAttribute("class", "curso c1")
-// novoElemento.innerHTML = "ReactNative"
 
-// caixa1.appendChild(novoElemento)
 
 cursos.map((el, i) => {
     const novoElemento = document.createElement("div");
