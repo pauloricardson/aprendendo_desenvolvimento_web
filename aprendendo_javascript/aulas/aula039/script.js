cbf04909f3ea65d5526fff722a5edca35843b1d0
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 11:09:04 -0300

    iniciando os edutos

--- aprendendo_javascript/aulas/aula039/script.js
+++ aprendendo_javascript/aulas/aula039/script.js
@@ -2,3 +2,5 @@ const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 
 console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+console.log()
