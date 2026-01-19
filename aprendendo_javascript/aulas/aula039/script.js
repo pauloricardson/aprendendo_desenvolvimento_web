1384d2f6568eb6c1b92a3cf2346487bf56d5ed04
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 18:49:05 -0300

    atualização

--- aprendendo_javascript/aulas/aula039/script.js
+++ aprendendo_javascript/aulas/aula039/script.js
@@ -3,7 +3,6 @@ const btn_c = [...document.querySelectorAll(".curso")]
 
 const c1_2 = document.querySelector('#c1_2')
 
-console.log(c1_2.parentNode.parentNode.parentNode.children[4])
 
 // if (caixa1.children.length > 0) {
 //     console.log("Tem filhos")
@@ -14,3 +13,7 @@ console.log(c1_2.parentNode.parentNode.parentNode.children[4])
 // console.log(caixa1.children.length > 0 ? "Possui Filhos" : "Não possui")
 
 //caixa1.firstElementChild.innerHTML = 'teste'
+
+const c2 = document.querySelector('#c2')
+
+console.log(c2.firstElementChild)
