93c8af8a3feaa9f2b373be45d1af625174cf4570
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 11:48:11 -0300

    Navegação e Manipulação Avançada do DOM em JavaScript

--- aprendendo_javascript/aulas/aula039/script.js
+++ aprendendo_javascript/aulas/aula039/script.js
@@ -1,6 +1,16 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 
-console.log(caixa1.hasChildNodes())
-console.log(btn_c[0].hasChildNodes())
-console.log()
+const c1_2 = document.querySelector('#c1_2')
+
+console.log(c1_2.parentNode.parentNode.parentNode.children[4])
+
+// if (caixa1.children.length > 0) {
+//     console.log("Tem filhos")
+// } else {
+//     console.log("Não tem filhos")
+// }
+
+// console.log(caixa1.children.length > 0 ? "Possui Filhos" : "Não possui")
+
+//caixa1.firstElementChild.innerHTML = 'teste'
