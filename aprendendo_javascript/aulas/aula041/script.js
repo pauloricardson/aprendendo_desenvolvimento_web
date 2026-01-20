0651e372e9814995a63733ea4994b6a0c00b2053
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-19 22:38:11 -0300

    preparando para a aula sobre remoção de elemento do DOM

---
+++ aprendendo_javascript/aulas/aula041/script.js
@@ -0,0 +1,13 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const cursos = ['teste' ,'HTML', 'CSS', 'Javascrip', 'PHP', 'React', 'MySQL', 'ReactNative']
+
+
+
+cursos.map((el, i) => {
+    const novoElemento = document.createElement("div");
+    novoElemento.setAttribute("id", "c" + (i + 1));
+    novoElemento.setAttribute("class", "curso c1")
+    novoElemento.innerHTML = (el)
+    caixa1.appendChild(novoElemento)
+})
