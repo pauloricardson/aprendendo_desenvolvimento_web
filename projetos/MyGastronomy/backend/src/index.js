abbd646a845aabe602e3bd049ea3ce316101ab87
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-21 19:58:52 -0300

    atualização

--- projetos/MyGastronomy/backend/src/index.js
+++ projetos/MyGastronomy/backend/src/index.js
@@ -2,7 +2,25 @@ import express from 'express'
 import cors from 'cors'
 
 async function main () {
+    const hostname = 'localhost'
+    const port = 3000
 
+    const app = express()
+
+    app.use(express.json())
+    app.use(cors())
+
+    app.get('/', (req, res) => {
+        res.send({
+            success: true,
+            statusCode: 200,
+            body: 'Welcome to MyGastronome!'
+        })
+    })
+
+    app.listen(port, () => {
+        console.log(`Server running on: http://${hostname}:${port}`)
+    })
 }
 
 main()
