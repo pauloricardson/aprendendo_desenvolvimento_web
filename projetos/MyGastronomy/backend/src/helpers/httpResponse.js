5c1dc1f7b734c06ebb459fdee37e6479b3619308
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-23 23:30:24 -0300

    atualização

--- projetos/MyGastronomy/backend/src/helpers/httpResponse.js
+++ projetos/MyGastronomy/backend/src/helpers/httpResponse.js
@@ -1,11 +1,23 @@
 export const ok = (body) => {
-
+    return {
+        success: true,
+        statusCode: 200,
+        body: body
+    }
 }
 
 export const notFound = (body) => {
-
+    return {
+        success: false,
+        statusCode: 400,
+        body: 'Not found'
+    }
 }
 
-export const serverError = (body) => {
-
+export const serverError = (error) => {
+    return {
+        success: false,
+        statusCode: 400,
+        body: error
+    }
 }
