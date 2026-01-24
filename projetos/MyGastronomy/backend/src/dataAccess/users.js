5d102b745ee0cfe72c5093ad5784bd4deb75e429
Paulo Ricardsonl <pauloricardsonsilvacosta@gmail.com>
2026-01-23 23:35:18 -0300

    dataAcces - users ok

--- projetos/MyGastronomy/backend/src/dataAccess/users.js
+++ projetos/MyGastronomy/backend/src/dataAccess/users.js
@@ -1,3 +1,24 @@
 import { Mongo } from "../database/mongo.js"
 import { ObjectId } from "mongodb"
 import crypto from 'crypto'
+
+const collectionName = 'users'
+
+export default class UsersDataAccess {
+    async getUsers() {
+        const result = await Mongo.db
+        .collection(collectionName)
+        .find({})
+        .toArray()
+
+        return result
+    }
+
+    async deleteUser() {
+
+    }
+
+    async updateUser() {
+
+    }
+}
